a56523c43630a17960e2d1c52a8d4ce5b49fca9a
locugh <2015137612@qq.com>
2019-07-08 17:35:21 +0800

    7-8

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import MintUi from "mint-ui";
+import "mint-ui/lib/style.css";
 
-Vue.config.productionTip = false
+
+Vue.config.productionTip = false;
+
+Vue.use(MintUi);
 
 new Vue({
   router,
